b45d4ec9599ace85292e2dfdb833940aa064b4bc
James Dixon <notjamesdixon@gmail.com>
2022-11-22 18:11:47 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,19 @@
-source .env
-hugo && rsync -avz --delete public/ ${USER}@${HOSTNAME}:~/blog/public/
+if [ -f "./.env" ]; then
+  . ./.env
+else
+  echo "Please create a .env file with a HUGO_USER, HUGO_HOSTNAME, and HUGO_OUTPUT_DIR variable"
+  exit
+fi
+
+if ! [ -x "$(command -v hugo)" ]; then
+  echo "Please install hugo and rerun script"
+  exit
+fi
+
+if ! [ -x "$(command -v rsync)" ]; then
+  echo "Please install rsync and rerun script"
+  exit
+fi
+
+echo Building and pushing to: ${HUGO_USER}@${HUGO_HOSTNAME}:${HUGO_OUTPUT_DIR}
+hugo && rsync -avz --delete public/ ${HUGO_USER}@${HUGO_HOSTNAME}:${HUGO_OUTPUT_DIR}
