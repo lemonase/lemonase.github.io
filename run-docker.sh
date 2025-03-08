2ce3c6479ae90ad2e666993f725526ab3860da5f
James Dixon <notjamesdixon@gmail.com>
2025-03-08 01:20:01 +0000

    add restart always flag to run script

--- run-docker.sh
+++ run-docker.sh
@@ -5,5 +5,5 @@ docker run -v "$PWD:/src" hugomods/hugo:latest build
 
 # run nginx server with output html
 docker rm blog-nginx
-docker run --name blog-nginx -v './public:/usr/share/nginx/html:ro' -p 8008:80 -d nginx
+docker run --name blog-nginx --restart always -v './public:/usr/share/nginx/html:ro' -p 8008:80 -d nginx
 
