ddbdf36a131d468470a21dcb1c82dbece48321a4
James Dixon <notjamesdixon@gmail.com>
2025-03-08 01:01:43 +0000

    add run docker script

---
+++ run-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# build public site with hugo
+docker run -v "$PWD:/src" hugomods/hugo:latest build 
+
+# run nginx server with output html
+docker rm blog-nginx
+docker run --name blog-nginx -v './public:/usr/share/nginx/html:ro' -p 8008:80 nginx
+
