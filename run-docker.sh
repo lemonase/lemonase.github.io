f6a96743a54e70f12caaa2f6698d83d37a6d4c7a
James Dixon <notjamesdixon@gmail.com>
2025-10-01 15:27:37 +0000

    update run script to use script dir

--- run-docker.sh
+++ run-docker.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# build public site with hugo
-docker run -v "$PWD:/src" hugomods/hugo:latest build 
+# get the correct directory (relative to this script)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# run nginx server with output html
+# build public site with latest hugo
+docker run -v "$SCRIPT_DIR:/src" hugomods/hugo:latest build
+
+# restart nginx docker image with updated html
+docker stop blog-nginx
 docker rm blog-nginx
-docker run --name blog-nginx --restart always -v './public:/usr/share/nginx/html:ro' -p 8008:80 -d nginx
+docker run --name blog-nginx --restart always -v "$SCRIPT_DIR/public:/usr/share/nginx/html:ro" -p 8008:80 -d nginx
 
